6212070a2b480c379031de83abef0dffc45fe618
Satwinder Singh <satwindersinghsaini59@gmail.com>
2022-04-10 00:48:50 -0400

    session code removed

--- keystone.ts
+++ keystone.ts
@@ -23,11 +23,8 @@ export default withAuth(
       url: 'file:./keystone.db',
     }, 
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-    ui: {
-      // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
+    
     lists,
-    session,
+    
   })
 );
